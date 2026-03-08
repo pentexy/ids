88e1a1da1d31ecae18436805b132e799dc70c917
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2026-03-08 12:14:28 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from handlers.start import start_handler
+from handlers.deposit import deposit_handler
+from handlers.owner import owner_notifier
+
+__all__ = ["start_handler", "deposit_handler", "owner_notifier"]
